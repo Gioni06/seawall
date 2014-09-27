b968c7fbad33f9786d1d3be448cdbb1e817af4ae
Jonas <jonas.duri@googlemail.com>
2014-09-27 16:45:06 +0200

    first commit

---
+++ app/scripts/controllers/login.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name applicationApp.controller:LoginCtrl
+ * @description
+ * # LoginCtrl
+ * Controller of the applicationApp
+ */
+angular.module('applicationApp')
+  .controller('LoginCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
